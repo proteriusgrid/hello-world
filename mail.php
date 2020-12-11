6253ce6377a4c0a0c4f4c79d9566ed6394798432
Grid <75792510+proteriusgrid@users.noreply.github.com>
2020-12-11 13:28:22 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?
+echo " Информация принята"
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+
+echo "Приветствую тебя,";
+echo $name;
+echo "Твой номер телефона";
+echo $phone
+
+
+?>
